7c4977d6ed2e6b59da16230fbeadfd38aa0cc5c7
FayadIdrissB <fayad-idriss.bacari@outlook.com>
2025-02-10 19:38:31 +0100

    modificated backend

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,47 @@
-const express = require('express')
-require('dotenv').config()
-const cors = require('cors')
+const http = require('http');
+const app = require('./app');
 
+const normalizePort = val => {
+  const port = parseInt(val, 10);
 
-const app = express();
-const PORT = process.env.PORT || 5002
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT ||'3000');
+app.set('port', port);
 
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
 
-app.use(cors())
-app.use(express.json())
+const server = http.createServer(app);
 
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
 
-app.get('/api/youtube', (_, res) => {
-    res.send({
-        msg: 'Hello les putes'
-    })
-})
-
-app.listen(PORT, () => {
-    console.log(`Le serveur est lancé sur le port : ${PORT}`)
-})
+server.listen(port);
