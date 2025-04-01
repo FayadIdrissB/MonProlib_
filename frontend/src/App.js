e48d165daf96989c751d5fde2a76ed12e93d1a0f
Aldrit <aldritkuleta2@gmail.com>
2025-04-01 15:46:20 +0200

    dev mise en place du frontend a moitie fonctionnelle

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-
-import Home from './Pages/home/home';
-import HomePro from './Pages/pro/home_pro/homePro.jsx';
-import RegisterPro from './Pages/pro/register_pro/registerPro'
-import RegisterUser from './Pages/user/register_user/registerUser.jsx'
-import Login from './Pages/login/login.jsx'
-import WelcomePro from './Pages/pro/welcome_pro/welcomePro.jsx';
-import WelcomeUser from './Pages/user/welcome_user/welcomeUser.jsx';
+import Home from "./Pages/home/home";
+import HomePro from "./Pages/pro/home_pro/homePro.jsx";
+import RegisterPro from "./Pages/pro/register_pro/registerPro";
+import RegisterUser from "./Pages/user/register_user/registerUser.jsx";
+import Login from "./Pages/login/login.jsx";
+import WelcomePro from "./Pages/pro/welcome_pro/welcomePro.jsx";
+import WelcomeUser from "./Pages/user/welcome_user/welcomeUser.jsx";
+import Calendar from "./Component/calendar/calendar.jsx";
 
 function App() {
   return (
@@ -18,6 +18,7 @@ function App() {
         <Route path="/register_pro" element={<RegisterPro />} />
         <Route path="/register_user" element={<RegisterUser />} />
         <Route path="/home_pro" element={<HomePro />} />
+        <Route path="/calendar_pro" element={<Calendar />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/welcome_pro/:id" element={<WelcomePro />} />
         <Route path="/welcome_user/:id" element={<WelcomeUser />} />
