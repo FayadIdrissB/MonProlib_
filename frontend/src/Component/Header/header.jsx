a300e3bcf43bb1116c25f9200ba7b23606406913
Aldrit <aldritkuleta2@gmail.com>
2025-04-02 09:10:12 +0200

    fix modification du css du calendrier et de la page en elle meme

--- frontend/src/Component/Header/header.jsx
+++ frontend/src/Component/Header/header.jsx
@@ -1,71 +1,90 @@
-import './header.css';
-import { useNavigate } from 'react-router-dom';
-import { useState, useEffect, useRef } from 'react';
+import "./header.css";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect, useRef } from "react";
 
-
-import Logo from '../../Image/logoP.png';
-
+import Logo from "../../image/logoP.png";
 
 function Header() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const [showDropdown, setShowDropdown] = useState(false);
+  const dropdownRef = useRef(null); // Référence pour détecter les clics en dehors
 
-    const [showDropdown, setShowDropdown] = useState(false);
-    const dropdownRef = useRef(null); // Référence pour détecter les clics en dehors
+  // Fonction pour basculer l'affichage du menu
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
 
+  // Fonction pour fermer le menu si on clique en dehors
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setShowDropdown(false);
+    }
+  };
 
-    // Fonction pour basculer l'affichage du menu
-    const toggleDropdown = () => {
-        setShowDropdown(!showDropdown);
+  // Ajout d'un écouteur d'événement pour détecter les clics en dehors
+  useEffect(() => {
+    if (showDropdown) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
     };
+  }, [showDropdown]);
 
-
-    // Fonction pour fermer le menu si on clique en dehors
-    const handleClickOutside = (event) => {
-        if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-            setShowDropdown(false);
-        }
-    };
-
-
-    // Ajout d'un écouteur d'événement pour détecter les clics en dehors
-    useEffect(() => {
-        if (showDropdown) {
-            document.addEventListener("mousedown", handleClickOutside);
-        } else {
-            document.removeEventListener("mousedown", handleClickOutside);
-        }
-
-        return () => {
-            document.removeEventListener("mousedown", handleClickOutside);
-        };
-    }, [showDropdown]);
-
-
-    return (
-        <div className="header">
-            <div className="header_container">
-                <div className='header_container_button-logo'>
-                    <img src={Logo} alt="" onClick={() => navigate('/')} className='logo_header_'/>  
-                </div>
-                <div className='header_container_button'>
-                    <button className='header_container_button_paragraphe'>À propos</button>
-                    <button className='header_container_button_paragraphe' onClick={() => navigate('/Login')}>Connexion</button>
-                    
-                    {/* Bouton "Get Started" avec gestion du menu au clic */}
-                    <button className='header_container_button_started' onClick={toggleDropdown} ref={dropdownRef}>
-                        Inscription
-                        {showDropdown && (
-                            <div className="menu">
-                                <button className="menu_item" onClick={() => navigate('/register_user')}>Compte Utilisateur</button>
-                                <button className="menu_item" onClick={() => navigate('/register_pro')}>Compte Pro</button>
-                            </div>
-                        )}
-                    </button>
-                </div>
-            </div>
+  return (
+    <div className="header">
+      <div className="header_container">
+        <div className="header_container_button-logo">
+          <img
+            src={Logo}
+            alt=""
+            onClick={() => navigate("/")}
+            className="logo_header_"
+          />
         </div>
-    );
+        <div className="header_container_button">
+          <button className="header_container_button_paragraphe">
+            À propos
+          </button>
+          <button
+            className="header_container_button_paragraphe"
+            onClick={() => navigate("/Login")}
+          >
+            Connexion
+          </button>
+
+          {/* Bouton "Get Started" avec gestion du menu au clic */}
+          <button
+            className="header_container_button_started"
+            onClick={toggleDropdown}
+            ref={dropdownRef}
+          >
+            Inscription
+            {showDropdown && (
+              <div className="menu">
+                <button
+                  className="menu_item"
+                  onClick={() => navigate("/register_user")}
+                >
+                  Compte Utilisateur
+                </button>
+                <button
+                  className="menu_item"
+                  onClick={() => navigate("/register_pro")}
+                >
+                  Compte Pro
+                </button>
+              </div>
+            )}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
