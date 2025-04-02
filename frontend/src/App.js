e59b81c9d2d3c307a9a4f5daa32f9f61a82e94e4
Aldrit <aldritkuleta2@gmail.com>
2025-04-02 09:10:12 +0200

    fix modification du css du calendrier et de la page en elle meme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ import RegisterUser from "./Pages/user/register_user/registerUser.jsx";
 import Login from "./Pages/login/login.jsx";
 import WelcomePro from "./Pages/pro/welcome_pro/welcomePro.jsx";
 import WelcomeUser from "./Pages/user/welcome_user/welcomeUser.jsx";
-import Calendar from "./Component/calendar/calendar.jsx";
+import CalendarPro from "./Pages/pro/calendar_pro/calendarPro";
 
 function App() {
   return (
@@ -18,7 +18,7 @@ function App() {
         <Route path="/register_pro" element={<RegisterPro />} />
         <Route path="/register_user" element={<RegisterUser />} />
         <Route path="/home_pro" element={<HomePro />} />
-        <Route path="/calendar_pro" element={<Calendar />} />
+        <Route path="/calendar_pro" element={<CalendarPro />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/welcome_pro/:id" element={<WelcomePro />} />
         <Route path="/welcome_user/:id" element={<WelcomeUser />} />
